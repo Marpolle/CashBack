36b1bd80b04378a4003e2d592cc41f58c39f244d
Maria Polezhaeva <mari.polezhaeva.97@mail.ru>
2024-07-11 12:09:57 +0300

    Add tests

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateCashBackIfAmountLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateCashBackSIfAmount1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+}
+
